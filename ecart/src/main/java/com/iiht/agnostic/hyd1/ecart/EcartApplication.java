6501f49816937752e2249c1fb71ab225a43c39b9
NITHYAKISHAN <NITHYAKISHAN@192.168.11.27>
2019-10-29 16:58:37 +0530

    First Commit By Nithya

---
+++ ecart/src/main/java/com/iiht/agnostic/hyd1/ecart/EcartApplication.java
@@ -0,0 +1,13 @@
+package com.iiht.agnostic.hyd1.ecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcartApplication.class, args);
+	}
+
+}
